8d6f390ec0228f59b8860a243ab274281a35cbd3
zBragaTo <leonardobragatobassi@hotmail.com>
2024-10-09 00:45:57 -0300

    Add Data Base

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,33 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.complemento = dados.complemento();
+        this.numero = dados.numero();
+    }
+}
