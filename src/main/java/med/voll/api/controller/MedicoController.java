dc8654086601b64b2346c6b2902e6524de03b24e
zBragaTo <leonardobragatobassi@hotmail.com>
2024-10-15 23:25:48 -0300

    Adding GET

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,10 +5,7 @@ import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -21,6 +18,11 @@ public class MedicoController {
     @PostMapping
     @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-            repository.save(new Medico(dados));
-      }
+        repository.save(new Medico(dados));
+    }
+
+    @GetMapping("/{id}")
+    public Medico dados(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
