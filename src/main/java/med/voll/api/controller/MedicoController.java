a54427801d624e55960cc546c44818c240839e0a
zBragato <leonardobragatobassi@hotmail.com>
2024-11-16 17:25:29 -0300

    Putting GET ALL

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -22,8 +25,8 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
 
-    @GetMapping("/{id}")
-    public Medico dados(@PathVariable Long id) {
-        return repository.findById(id).orElse(null);
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
